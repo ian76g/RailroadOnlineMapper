365680b1803049ae661090b7a2a17e873ec6aa2a
ian76g <github@pordi.de>
2021-11-14 18:18:43 +0100

    I want to have all images in same column to have multi line cursor to change all images at once

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require_once 'config.php';
     }
 
     $tableHeader = '<thead>
-                        <th style="background-color: beige"><img height="28" width="40" src="images/player.svg"></th>
+                        <th style="background-color: beige">                                                        <img height="28" width="40" src="images/player.svg"></th>
                         <th style="background-color: beige"><A href="?sortby=0&sortorder=desc" style="color: white"><img height="28" width="40" src="images/distance.svg"></A></th>
                         <th style="background-color: beige"><A href="?sortby=1&sortorder=desc" style="color: white"><img height="28" width="40" src="images/switch.svg"></A></th>
                         <th style="background-color: beige"><A href="?sortby=6&sortorder=desc" style="color: white"><img height="28" width="40" src="images/tree.svg"></A></th>
