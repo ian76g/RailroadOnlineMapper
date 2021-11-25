6d2b09829185aa22660bc2e4f3c25f0feb4ebc98
ian76g <github@pordi.de>
2021-11-25 23:31:52 +0100

    bugfix to bugfix to prevent cache

--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,6 @@
     <meta charset="UTF8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title><?= isset($PageTitle) ? $PageTitle : "RailroadsOnlineMapper"?></title>
-    <link rel="stylesheet" href="/assets/css/reset.css?<?php echo filemtime('../assets/css/reset.css');?>">
-    <link rel="stylesheet" href="/assets/css/main.css?<?php echo filemtime('../assets/css/main.css');?>">
+    <link rel="stylesheet" href="/assets/css/reset.css?<?php echo filemtime('assets/css/reset.css');?>">
+    <link rel="stylesheet" href="/assets/css/main.css?<?php echo filemtime('assets/css/main.css');?>">
 </head>
