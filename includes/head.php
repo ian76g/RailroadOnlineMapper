40a18783e40010f7adda7fc90efd59e527bd4ff3
iScripters <iScripters@gmail.com>
2021-11-26 18:50:32 +0100

    Added datatables to the index page

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,9 @@
 <head>
     <meta charset="UTF8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title><?= isset($PageTitle) ? $PageTitle : "RailroadsOnlineMapper"?></title>
-    <link rel="stylesheet" href="/assets/css/reset.css?<?php echo filemtime('assets/css/reset.css');?>">
-    <link rel="stylesheet" href="/assets/css/main.css?<?php echo filemtime('assets/css/main.css');?>">
+    <title><?= $PageTitle ?? "RailroadsOnlineMapper" ?></title>
+    <link rel="stylesheet" href="/assets/css/reset.css?<?php echo filemtime('assets/css/reset.css');?>" type="text/css">
+    <link rel="stylesheet" href="/assets/css/main.css?<?php echo filemtime('assets/css/main.css');?>" type="text/css">
+    <link rel="stylesheet" href="/assets/css/datatables.css?<?php echo filemtime('assets/css/main.css');?>" type="text/css">
+    <script type="text/javascript" src="/assets/js/datatables.min.js"></script>
 </head>
