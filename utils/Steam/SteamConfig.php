8c44246e290ad44380076149740680d36ab88cc5
ian76g <github@pordi.de>
2021-12-27 14:01:27 +0100

    removed steam API key

--- utils/Steam/SteamConfig.php
+++ utils/Steam/SteamConfig.php
@@ -1,6 +1,7 @@
 <?php
+include ('steamAPIKey.php');
 //Version 4.0
-$steamauth['apikey'] = "65C91F1ED36271523507CEF64CDE51C5"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['apikey'] = $steamAPIKey; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "minizwerg.online"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "/index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "/index.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
